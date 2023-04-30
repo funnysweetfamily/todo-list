47dfc310aa6d57e1e5a0ad7fe1bfd8a8d3b65db1
Илья <shokinilya@email.com>
2023-04-30 20:53:15 +0200

    исправление багов(проект рабочий)

--- app.py
+++ app.py
@@ -7,68 +7,56 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
 
-class Task(db.Model):
+class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), primary_key=False)
     intro = db.Column(db.String(300), primary_key=False)
     text = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
-
-@app.route('/', , methods=['GET'])
+@app.route('/')
 @app.route('/home')
 def index():
     return render_template("index.html")
 
 
-@app.route('/tasks', methods=['GET'])
-def tasks():
-    articles = Task.query.order_by(Article.date.desc()).all()
+
+@app.route('/tasks')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
     return render_template("posts.html", articles=articles)
 
 
-@app.route('/tasks/<int:id>', methods=['GET'])
+@app.route('/tasks/<int:id>')
 def post_detail(id):
-    task = Task.query.get(id)
+    article = Article.query.get(id)
     return render_template("post_detail.html", article=article)
 
 
-@app.route('/task/<id>/', methods=['DELETE'])
+@app.route('/tasks/<int:id>/del')
 def post_delete(id):
-    task = Task.query.get_or_404(id)
+    article = Article.query.get_or_404(id)
 
     try:
-        db.session.delete(task)
+        db.session.delete(article)
         db.session.commit()
-        return redirect('/posts')
+        return redirect('/tasks')
     except:
         return "Oшибка"
 
-@app.route('/tasks', methods=['POST'])
+@app.route('/create-tasks', methods=['POST','GET'])
 def create_article():
     if request.method== 'POST':
         title = request.form['title']
         intro = request.form['intro']
         text = request.form['text']
-        title= Task(title=title,intro=intro, text=text)
+        article = Article(title=title,intro=intro, text=text)
         try:
-            db.session.add(task)
-            db.session.commit()
-            return redirect('/posts')
-        except:
-            return "Oшибка "
-
-    
-@app.route('/task/<id>', methods=['PUT'])
-def create_article():
-    if request.method== 'POST':
-        title = request.form['title']
-        intro = request.form['intro']
-        text = request.form['text']
-        title= Task(title=title,intro=intro, text=text)
-        try:
-            db.session.add(task)
+            db.session.add(article)
             db.session.commit()
             return redirect('/tasks')
         except:
@@ -77,25 +65,24 @@ def create_article():
     else:
         return render_template ("create-article.html")
 
-@app.route('/task/<id>/change', methods=['GET'])
+@app.route('/tasks/<int:id>/update', methods=['POST','GET'])
 def post_update(id):
+    article = Article.query.get(id)
     if request.method== 'POST':
-        task.title = request.form['title']
-        task.intro = request.form['intro']
-        task.text = request.form['text']
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
 
         try:
             db.session.commit()
-            return redirect('/posts')
+            return redirect('/tasks')
         except:
             return "Oшибка "
 
     else:
-        return render_template ("post_update.html", task=task)
+        return render_template ("post_update.html", article=article)
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-   
-
